57db18e9e2fc90282e7c9350382f6b4875736392
Nasker <xamps23@gmail.com>
2024-02-22 20:07:23 +0100

    All examples updated

--- Test/UltraSound.py
+++ Test/UltraSound.py
@@ -6,11 +6,7 @@ ECHO_PIN = 27
 
 
 def main():
-    # Define GPIO pin constants.
-
-    # Initialise Sensor with pins, speed of sound.
     speed_of_sound = 315
-    # Measure Distance 5 times, return average.
     echo = Echo(TRIGGER_PIN, ECHO_PIN, speed_of_sound)
     samples = 1
     while True:
