35b12d227706f0808cdf45da7d141bfd850d4f92
Nasker <xamps23@gmail.com>
2024-04-17 16:01:53 +0200

    Updated server

--- WebServer/server.py
+++ WebServer/server.py
@@ -1,11 +1,19 @@
 from flask import Flask, jsonify, render_template
-# import adafruit_dht
-# import board
+import adafruit_dht
+import board
+import RPi.GPIO as GPIO
+
+DHT_PIN = board.D18
+RELAY_PIN = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+
 
 app = Flask(__name__)
 
 # Initialize the DHT device
-# dhtDevice = adafruit_dht.DHT11(board.D18)
+dhtDevice = adafruit_dht.DHT11(DHT_PIN)
 
 
 @app.route('/')
@@ -16,8 +24,8 @@ def home():
 @app.route('/api/sensor')
 def sensor_data():
     try:
-        temperature_c = 23.0  # dhtDevice.temperature
-        humidity = 53.0  # dhtDevice.humidity
+        temperature_c = dhtDevice.temperature
+        humidity = dhtDevice.humidity
         return jsonify({'temperature': temperature_c, 'humidity': humidity})
     except RuntimeError as error:
         return jsonify({'error': str(error)})
@@ -25,9 +33,10 @@ def sensor_data():
 
 @app.route('/api/toggle_relay', methods=['POST'])
 def toggle_relay():
+    GPIO.output(RELAY_PIN, not GPIO.input(RELAY_PIN))
     print('Relay toggled')
     return 'Relay toggled'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
